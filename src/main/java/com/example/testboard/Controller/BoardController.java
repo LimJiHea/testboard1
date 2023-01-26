d32add66ba0432ea8d3af1061be9981f126f230e
LJH <akdi22@hanmail.net>
2023-01-27 03:32:34 +0900

    commit

--- src/main/java/com/example/testboard/Controller/BoardController.java
+++ src/main/java/com/example/testboard/Controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.example.testboard.Controller;
 
+import com.example.testboard.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,9 +13,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor        //생성자주입
 public class BoardController {
 
+    private final BoardService service;
+
     @GetMapping("/hello")
     public String Hello(){
         return "/boards/hello";
     }
 
+    @GetMapping("/test")
+    public String test(Model model){
+        model.addAttribute("cnt", service.boardCount());
+        model.addAttribute("test",service.boardList());
+
+        return "/boards/hello";
+
+    }
 }
